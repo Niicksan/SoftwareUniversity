5bdd0cd52aa0f7ab716d52ba3b8d1165b2255d9d
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-01-13 13:49:49 +0200

    03. Simple Conditions

---
+++ Programming Basics/Programming Basics - C#/Exercises/03. Simple Conditions/03. Simple Conditions/Seconds/Seconds.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstCompetotpr = int.Parse(Console.ReadLine());
+            int secondCompetotpr = int.Parse(Console.ReadLine());
+            int thirdCompetotpr = int.Parse(Console.ReadLine());
+
+            int seconds = firstCompetotpr + secondCompetotpr + thirdCompetotpr;
+            int minutes = 0;
+
+            if (seconds > 59)
+            {
+                minutes++;
+                seconds = seconds - 60;
+            }
+
+            if (seconds > 59)
+            {
+                minutes++;
+                seconds = seconds - 60;
+            }
+
+            if (seconds < 10)
+            {
+                Console.WriteLine(minutes + ":" + "0" + seconds);
+            }
+
+            else
+            {
+                Console.WriteLine(minutes + ":" + seconds);
+            }
+        }
+    }
+}
---
+++ Programming Basics/Programming Basics - C#/Exercises/03. Simple Conditions/03. Simple Conditions/Transport Price/Transport Price.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transport_Price
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var time = Console.ReadLine().ToLower();
+
+            var taxiBegingPrice = 0.70;
+            var taxiDayPrice = 0.79;
+            var taxiNightPrice = 0.90;
+
+            var bus = 0.09;
+            var train = 0.06;
+
+            if (n < 20)
+            {
+                if (time == "day")
+                {
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
