fc581da0c6b1e201229625b37c696478256b67cf
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-01-13 12:32:39 +0200

    03. Nums 1 to 20

---
+++ Programming Basics/Programming Basics - C#/Exercises/01.First step in Coding/01.First step in Coding/03. Nums 1 to 20/Nums 1 to 20.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Numbers_1_To_20
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
