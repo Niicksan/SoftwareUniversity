cf895490f6bb6c8446c0992f7c9b003259cc5bdf
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-01-13 13:46:36 +0200

    03. Simple Conditions

---
+++ Programming Basics/Programming Basics - C#/Exercises/03. Simple Conditions/03. Simple Conditions/Password checker/Password checker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pass = (Console.ReadLine());
+            var password = "s3cr3t!P@ssw0rd";
+
+            if (pass == password)
+            {
+                Console.WriteLine("Wellcome");
+            }
+
+            else
+	        {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
