1bfe52c8eaa2b344ac08cf1be3b80488a60a4c7b
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-01-13 12:34:09 +0200

    05. Rectangle Area

---
+++ Programming Basics/Programming Basics - C#/Exercises/01.First step in Coding/01.First step in Coding/05. Rectangle Area/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Arean_of_a_triangle
+{
+    class Program
+    {
+        static void Main()
+        {
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+
+            dicimal area;
+            area = a * b;
+
+            Console.WriteLine(area);
+
+        }
+    }
+}
---
+++ Programming Basics/Programming Basics - C#/Exercises/01.First step in Coding/01.First step in Coding/05. Rectangle Area/Rectangle Area.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Arean_of_a_triangle
+{
+    class Program
+    {
+        static void Main()
+        {
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+
+            double area;
+            area = a * b;
+            Console.WriteLine(area);
+
+        }
+    }
+}
