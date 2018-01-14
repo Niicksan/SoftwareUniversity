86a283b091c4636bc9717ddbbb9b058671119a38
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-01-14 10:55:08 +0200

    4. Complex Conditionals

---
+++ Programming Basics/Programming Basics - C#/Exercises/04. Complex Conditionals/04. Complex Conditionals/Operations Between Numbers/Operations Between Numbers.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operations_Between_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            string oper = Console.ReadLine();
+            string evenOdd;
+
+            double result;
+
+            if (oper == "+" || oper == "-" || oper == "*")
+            {
+
+                if (oper == "+")
+                {
+                    result = num1 + num2;
+                }
+
+                else if (oper == "-")
+                {
+                    result = num1 - num2;
+                }
+
+                else
+                {
+                    result = num1 * num2;
+
+                }
+
+                if (result % 2 == 0)
+                {
+                    evenOdd = "even";
+                }
+
+                else
+                {
+                    evenOdd = "odd";
+                }
+
+                Console.WriteLine("{0} {1} {2} = {3} - {4}, num1", oper, num2, result, evenOdd);
+            }
+
+
+            else if (oper == "/" || oper == "%")
+            {
+                if (num2 == 0)
+                {
+                    Console.WriteLine("Cannot divide {0} by zero", num1);
+                }
+
+                else
+                {
+                    if (oper == "/")
+                    {
+                        result = (num1 * 1.0) / (num2 * 1.0);
+                        Console.WriteLine("{0} / {1} = {2:f2}", num1, num2, result);
+                    }
+
+
+                    else
+                    {
+                        result = num1 % num2;
+                        Console.WriteLine("{0} {1} {2} = {3:0}", num1, oper, num2, result);
+                    }
+                }
+            }
+    }
+}
---
+++ Programming Basics/Programming Basics - C#/Exercises/04. Complex Conditionals/04. Complex Conditionals/Small-Shop/Small-Shop.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Small_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            input:
+            var product = Console.ReadLine().ToLower();
+            var city = Console.ReadLine().ToLower();
+            var quantity = double.Parse(Console.ReadLine());
+
+            
+            if (product == "coffee")
+            {
+                if (city == "sofia")
+                {
+                    Console.WriteLine(0.50 * quantity);
+                }
+                else if (city == "plovdiv")
+                {
+                    Console.WriteLine(0.40 * quantity);
+                }
+                else if (city == "varna")
+                {
+                    Console.WriteLine(0.45 * quantity);
+                }
+            }
+
+            else if (product == "water")
+            {
+                if (city == "sofia")
+                {
+                    Console.WriteLine(0.80 * quantity);
+                }
+                else if (city == "plovdiv")
+                {
+                    Console.WriteLine(0.70 * quantity);
+                }
+                else if (city == "varna")
+                {
+                    Console.WriteLine(0.70 * quantity);
+                }
+            }
+
+            else if (product == "beer")
+            {
+                if (city == "sofia")
+                {
+                    Console.WriteLine(1.20 * quantity);
+                }
+                else if (city == "plovdiv")
+                {
+                    Console.WriteLine(1.15 * quantity);
+                }
+                else if (city == "varna")
+                {
+                    Console.WriteLine(1.10 * quantity);
+                }
+            }
+
+            else if (product == "sweets")
+            {
+                if (city == "sofia")
+                {
+                    Console.WriteLine(1.45 * quantity);
+                }
+                else if (city == "plovdiv")
+                {
+                    Console.WriteLine(1.30 * quantity);
+                }
+                else if (city == "varna")
+                {
+                    Console.WriteLine(1.35 * quantity);
+                }
+            }
+
+            else if (product == "peanuts")
+            {
+                if (city == "sofia")
+                {
+                    Console.WriteLine(1.60 * quantity);
+                }
+                else if (city == "plovdiv")
+                {
+                    Console.WriteLine(1.50 * quantity);
+                }
+                else if (city == "varna")
+                {
+                    Console.WriteLine(1.55 * quantity);
+                }
+            }
+
+            goto input;
+        }
+    }
+}
