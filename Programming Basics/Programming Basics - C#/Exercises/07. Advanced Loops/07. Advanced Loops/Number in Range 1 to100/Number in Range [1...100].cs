d9234fb210c31c2372c93064a6323f91efe0ac52
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-01-14 11:42:16 +0200

    7. Advanced Loops

---
+++ Programming Basics/Programming Basics - C#/Exercises/07. Advanced Loops/07. Advanced Loops/Number in Range 1 to100/Number in Range [1...100].cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_in_Range_1_to_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Еnter a number in the range [1...100]: ");
+            int n = int.Parse(Console.ReadLine());
+
+            while (n <= 0 || n >= 100)
+            {
+                Console.WriteLine("Invalid number!");
+                Console.Write("Еnter a number in the range [1...100]: ");
+                n = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("The number is: {0}", n);
+        }
+    }
+}
---
+++ Programming Basics/Programming Basics - C#/Exercises/07. Advanced Loops/07. Advanced Loops/Number table/Number table.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+           // int rows = n;
+
+            for (int row = 1; row <= n; row++)
+            {
+                int counter = 0;
+                for (int col = row; col <= n; col++)
+                {
+                    counter++;
+                    Console.Write(col + " ");
+                }
+
+                //int leftToPrint = n - counter;
+                int num = n - 1;
+
+                while (counter != n)
+                {
+                    Console.Write(num + " ");
+                    counter++;
+                    num--;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
