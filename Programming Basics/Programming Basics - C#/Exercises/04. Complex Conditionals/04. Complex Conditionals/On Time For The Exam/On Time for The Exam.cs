7968ab2c895e6d465e0e18d401b42b32d4d54945
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-01-14 10:58:38 +0200

    4. Complex Conditionals

---
+++ Programming Basics/Programming Basics - C#/Exercises/04. Complex Conditionals/04. Complex Conditionals/On Time For The Exam/On Time for The Exam.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace On_Time_For_The_Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            nt hourExam = int.Parse(Console.ReadLine());
+            int minExam = int.Parse(Console.ReadLine());
+            int hourArrival = int.Parse(Console.ReadLine());
+            int minArrival = int.Parse(Console.ReadLine());
+
+            int timeExam = (hourExam * 60) + minExam;
+            int timeArrival = (hourArrival * 60) + minArrival;
+
+            int timeInterval = (timeExam - timeArrival);
+
+            if (timeInterval >= 0 && timeInterval <= 30)
+            {
+                Console.WriteLine("On Time");
+                if (timeInterval == 0)
+                {
+                    Console.WriteLine();
+                }
+
+                else
+                {
+                    Console.WriteLine("{0} minutes before the start", timeInterval);
+                }
+            }
+
+            else if (timeInterval > 30)
+            {
+                Console.WriteLine("Early");
+                if (timeInterval < 60)
+                {
+                    Console.WriteLine("{0} minutes before the start", timeInterval);
+                }
+
+                else
+                {
+                    Console.WriteLine("{0}:{1:00} hours before the start", timeInterval / 60, timeInterval % 60);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("late");
+                timeInterval = -timeInterval;
+
+                if (timeInterval < 60)
+                {
+                    Console.WriteLine("{0} minutes after the start", timeInterval);
+                }
+
+                else
+                {
+                    Console.WriteLine("{0}:{1:00} hours after the start", timeInterval / 60, timeInterval % 60);
+                }
+            }
+        }
+    }
+}
