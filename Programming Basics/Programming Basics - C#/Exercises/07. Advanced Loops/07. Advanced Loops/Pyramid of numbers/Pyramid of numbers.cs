870d08a7be7e48c37d49b2e7b0d7b67dff2922b2
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-01-14 11:56:22 +0200

    7. Advanced Loops

---
+++ Programming Basics/Programming Basics - C#/Exercises/07. Advanced Loops/07. Advanced Loops/Pyramid of numbers/Pyramid of numbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyramid_of_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int row = 1;
+
+            int num = 1;
+            while (num <= n)
+            {
+                for (int i = 1; i < row;  i++)
+                {
+                    if (num == n)
+                    {
+                        break;
+                    }
+
+                    Console.Write(num + " ");
+                    num++;
+                }
+
+                Console.WriteLine(num);
+                num++;
+                row++;
+            }
+        }
+    }
+}
---
+++ Programming Basics/Programming Basics - C#/Exercises/07. Advanced Loops/07. Advanced Loops/Sum Digits/Sum Digits.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long sum = 0;
+
+            do
+            {
+                int dig = n % 10;
+                sum += dig;
+                n /= 10;
+            }
+            while (n > 0);
+        
+            Console.WriteLine(sum);
+        }
+    }
+}
