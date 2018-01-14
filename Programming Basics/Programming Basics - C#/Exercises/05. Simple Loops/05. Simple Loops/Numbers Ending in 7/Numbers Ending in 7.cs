cc195fb9ce01e523705c2df45be8c1eecc869f53
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-01-14 11:18:08 +0200

    5. Simple Loops

---
+++ Programming Basics/Programming Basics - C#/Exercises/05. Simple Loops/05. Simple Loops/Numbers Ending in 7/Numbers Ending in 7.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers_Ending_in_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 1; n < 1000; n++)
+            {
+                if (n % 10 == 7)
+                {
+                    Console.WriteLine(n);
+                }
+            }
+        }
+    }
+}
