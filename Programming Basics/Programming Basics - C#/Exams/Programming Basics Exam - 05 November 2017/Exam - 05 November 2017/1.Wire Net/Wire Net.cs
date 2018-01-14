74e2567edd1f13ecf9a83e71de49fbd32c3e2147
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-01-14 13:11:14 +0200

    Programming Basics Exam - 05 November 2017

---
+++ Programming Basics/Programming Basics - C#/Exams/Programming Basics Exam - 05 November 2017/Exam - 05 November 2017/1.Wire Net/Wire Net.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Wire_Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aMeters = int.Parse(Console.ReadLine());
+            int bMeters = int.Parse(Console.ReadLine());
+
+            double hNet = double.Parse(Console.ReadLine());
+            double price = double.Parse(Console.ReadLine());
+            double weight = double.Parse(Console.ReadLine());
+
+            double longNet = 2 * (aMeters + bMeters);
+            double priceNet = longNet * price;
+            double areaNet = longNet * hNet;
+            double weightNet = areaNet * weight;
+
+            Console.WriteLine(longNet);
+            Console.WriteLine(string.Format("{0:f2}", priceNet));
+            Console.WriteLine(string.Format("{0:f3}", weightNet));
+        }
+    }
+}
---
+++ Programming Basics/Programming Basics - C#/Exams/Programming Basics Exam - 05 November 2017/Exam - 05 November 2017/2.Lutenica/Lutenica.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Lutenica
+{
+    class Lutenica
+    {
+        static void Main(string[] args)
+        {
+            double kgTomatos = double.Parse(Console.ReadLine());
+            int kasetki = int.Parse(Console.ReadLine());
+            int jars = int.Parse(Console.ReadLine());
+
+            double kgLutenica = kgTomatos / 5;
+            double jarsCount = kgLutenica / 0.535;
+            double boxes = jarsCount / jars;
+            double difference = Math.Floor(Math.Abs(kasetki - boxes));
+            double diffjars = Math.Floor(Math.Abs(jarsCount - (kasetki * jars)));
+
+            if (boxes > kasetki)
+            {
+                Console.WriteLine("Total lutenica: {0} kilograms.", Math.Floor(kgLutenica));
+                Console.WriteLine("{0} boxes left.", difference);
+                Console.WriteLine("{0} jars left.", diffjars);
+            }
+
+            else
+            {
+                Console.WriteLine("Total lutenica: {0} kilograms.", Math.Floor(kgLutenica));
+                Console.WriteLine("{0} more boxes needed.", difference);
+                Console.WriteLine("{0} more jars needed.", diffjars);
+            }
+        }
+    }
+}
