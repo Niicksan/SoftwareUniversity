d76d5fa4c47d9661bfe40de130965dff43b35f3b
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-01-14 10:56:46 +0200

    4. Complex Conditionals

---
+++ Programming Basics/Programming Basics - C#/Exercises/04. Complex Conditionals/04. Complex Conditionals/Trip/Trip.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trip
+{
+    class Trip
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            string destination = "";
+            double moneyNeeded = 0;
+            string place = "";
+
+            if (budget <= 100)
+            {
+                destination = "Bulgaria";
+                if (season == "summer")
+                {
+                    moneyNeeded = budget * 0.3;
+                    place = "Camp";
+                }
+                else if (season == "winter")
+                {
+                    moneyNeeded = budget * 0.7;
+                    place = "Hotel";
+                }
+            }
+
+            else if (budget > 100 && budget <= 1000)
+            {
+                destination = "Balkans";
+                if (season == "summer")
+                {
+                    moneyNeeded = budget * 0.4;
+                    place = "Camp";
+                }
+                else if (season == "winter")
+                {
+                    moneyNeeded = budget * 0.8;
+                    place = "Hotel";
+                }
+            }
+
+            else if (budget > 1000)
+            {
+                destination = "Europe";
+                place = "Hotel";
+                moneyNeeded = budget * 0.9;
+            }
+
+            Console.WriteLine("Somewhere in {0}", destination);
+            Console.WriteLine("{0} - {1:0.00}", place, moneyNeeded);
+        }
+    }
+}
---
+++ Programming Basics/Programming Basics - C#/Exercises/04. Complex Conditionals/04. Complex Conditionals/Volleyball/Volleyball.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string year = Console.ReadLine().ToLower();
+            int holidays = int.Parse(Console.ReadLine());
+            int weekendsHome = int.Parse(Console.ReadLine());
+
+            int sofiaWeekend = 48 - weekendsHome;
+            double playSofia = 3.0 * sofiaWeekend / 4 * 2.0 * holidays / 4;
+            int playHome = weekendsHome;
+            double playTotal = playSofia + playHome;
+
+            if (year.Equals("leap"))
+            {
+                playTotal = Math.Truncate(playTotal * 15 / 100 + playTotal);
+            }
+
+            else if (year.Equals("normal"))
+            {
+                playTotal = Math.Truncate(playTotal);
+            }
+
+            Console.WriteLine(playTotal);
+
+        }
+    }
+}
