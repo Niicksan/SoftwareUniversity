f4fe0bae7934f062279c6220bcb94ef4ab9695ca
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-04-08 10:34:42 +0300

    Programming Basics Exam - 21 February 2016

---
+++ Programming Basics/Programming Basics - C#/Old Exams/Programming Basics Exam - 21 February 2016/03 - Fallen In Love/Fallen In Love.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03___Fallen_In_Love
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sign = 0;
+            int firstDots = 2 * n;
+            int middleDots = 0;
+            int secondDots = 1;
+
+            for (int row = 1; row <= n; row++)
+            {
+                Console.Write("#");
+                Console.Write(new string('~', sign));
+                Console.Write("#");
+                Console.Write(new string('.', firstDots));
+                Console.Write("#");
+                Console.Write(new string('.', middleDots));
+                Console.Write("#");
+                Console.Write(new string('.', firstDots));
+                Console.Write("#");
+                Console.Write(new string('~', sign));
+                Console.WriteLine("#");
+                sign++;
+                firstDots -= 2;
+                middleDots += 2;
+            }
+
+            for (int row = 0; row <= n; row++)
+            {
+                Console.Write(new string('.', secondDots));
+                Console.Write("#");
+                Console.Write(new string('~', sign));
+                Console.Write("#");
+                Console.Write(new string('.', middleDots));
+                Console.Write("#");
+                Console.Write(new string('~', sign));
+                Console.Write("#");
+                Console.WriteLine(new string('.', secondDots));
+                secondDots += 2;
+                sign--;
+                middleDots -= 2;
+            }
+            secondDots--;
+            for (int row = 1; row <=n + 1 ; row++)
+            {
+                Console.Write(new string('.', secondDots));
+                Console.Write(new string('#', 2));
+                Console.WriteLine(new string('.', secondDots));
+            }
+        }
+    }
+}
---
+++ Programming Basics/Programming Basics - C#/Old Exams/Programming Basics Exam - 21 February 2016/05 - Salt And Pepper/Salt And Pepper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05___Salt_And_Pepper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong number = ulong.Parse(Console.ReadLine());
+            string comand = Console.ReadLine();
+
+            while (comand != "end")
+            {
+                string[] comandElemnt = comand.Split(' ');
+                string action = comandElemnt[0];
+                int step = int.Parse(comandElemnt[1]);
+
+                if (action == "salt")
+                {
+                    for (int i = 0; i <= 63; i += step)
+                    {
+                        if (((number >> i) & 1) == 1)
+                        {
+                            ulong mask = ~((ulong)1 << i);
+                            number = number & mask;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i <= 63; i += step)
+                    {
+                        if (((number >> i) & 1) == 0)
+                        {
+                            ulong mask = ~(ulong)1 << i;
+                            number = number | mask;
+                        }
+                    }
+                }
+                comand = Console.ReadLine();
+            }
+            Console.WriteLine(number);
+        }
+    }
+}
