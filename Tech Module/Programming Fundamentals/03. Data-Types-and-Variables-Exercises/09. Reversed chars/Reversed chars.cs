b2ed97ecbacd3f11bcb436f3790e5e8225ddd190
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-05-26 01:31:29 +0300

    03. Data Types and Variables - Exercises

---
+++ Tech Module/Programming Fundamentals/03. Data-Types-and-Variables-Exercises/09. Reversed chars/Reversed chars.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _09._Reversed_chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstLetter = char.Parse(Console.ReadLine());
+            char secondLetter = char.Parse(Console.ReadLine());
+            char thirdLetter = char.Parse(Console.ReadLine());
+
+            string word = "" + firstLetter + secondLetter + thirdLetter;
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                Console.Write(word[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Tech Module/Programming Fundamentals/03. Data-Types-and-Variables-Exercises/13. Vowel or Digit/Vowel or Digit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _13._Vowel_or_Digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string symbol = Console.ReadLine().ToLower();
+
+            int number = 0;
+
+            bool isDigit = int.TryParse(symbol, out number);
+
+            if (isDigit)
+            {
+                Console.WriteLine("digit");
+            }
+            else if (symbol == "a" || symbol == "e" || symbol == "i" || symbol == "o" || symbol == "u")
+            {
+                Console.WriteLine("vowel");
+            }
+            else
+            {
+                Console.WriteLine("other");
+            }
+        }
+    }
+}
