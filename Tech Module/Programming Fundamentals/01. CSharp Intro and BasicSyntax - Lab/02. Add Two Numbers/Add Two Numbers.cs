d1f4c2ec175df1e9a708b05fcbae14c0bb3ffb09
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-05-26 00:35:11 +0300

    01. CSharp Intro and BasicSyntax - Lab

---
+++ Tech Module/Programming Fundamentals/01. CSharp Intro and BasicSyntax - Lab/02. Add Two Numbers/Add Two Numbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02._Add_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int result = num1 + num2;
+
+            Console.WriteLine($"{num1} + {num2} = {result}");
+        }
+    }
+}
