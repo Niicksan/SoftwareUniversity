691102a5c2b02d02e3795bd1d6412c93aa2204f6
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-05-26 00:53:35 +0300

    02. CSharp Conditional Statements and Loops - Lab

---
+++ Tech Module/Programming Fundamentals/02. CSharp Conditional Statements and Loops - Lab/11. Odd Number/Odd Number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _11._Odd_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Math.Abs(int.Parse(Console.ReadLine()));
+
+            while (n % 2 != 1)
+            {
+                Console.WriteLine("Please write an odd number.");
+                n = Math.Abs(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine($"The number is: {n}");
+        }
+    }
+}
