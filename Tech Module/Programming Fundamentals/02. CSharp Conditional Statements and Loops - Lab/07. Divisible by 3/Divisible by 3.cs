6fb89208a332505444bead48ffe0688d1e37629b
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-05-26 00:49:29 +0300

    02. CSharp Conditional Statements and Loops - Lab

---
+++ Tech Module/Programming Fundamentals/02. CSharp Conditional Statements and Loops - Lab/07. Divisible by 3/Divisible by 3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _07._Divisible_by_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3;
+            while (i <= 100)
+            {
+                Console.WriteLine(i);
+                i += 3;
+            }
+        }
+    }
+}
