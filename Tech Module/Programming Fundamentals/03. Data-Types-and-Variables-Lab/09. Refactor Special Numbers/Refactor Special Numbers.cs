684268b474d61bb0b810eb9efd3e57fb38415cb5
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-05-26 01:20:35 +0300

    03. Data Types and Variables - Lab

---
+++ Tech Module/Programming Fundamentals/03. Data-Types-and-Variables-Lab/09. Refactor Special Numbers/Refactor Special Numbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _09._Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int digit = i;
+                int sum = 0;
+
+                while (digit > 0)
+                {
+                    sum += digit % 10;
+                    digit /= 10;
+                }
+                bool special = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{i} -> {special}");
+            }
+        }
+    }
+}
