918066088943b50b0b80100dc51ad10da038bee2
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-05-26 00:44:00 +0300

    01. CSharp Intro and Basic Syntax - Exercises

---
+++ Tech Module/Programming Fundamentals/01. CSharp Intro and Basic Syntax - Exercises/04. Beverage Labels/Beverage Labels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04._Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            double energy = double.Parse(Console.ReadLine());
+            double sugar = double.Parse(Console.ReadLine());
+
+            double energyContent = volume * (energy / 100);
+            double sugarContent = volume * (sugar / 100);
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{energyContent}kcal, {sugarContent}g sugars");
+        }
+    }
+}
