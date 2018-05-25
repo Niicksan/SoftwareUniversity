1c8521cd64bd6ddb94e9149229aaa40a37f5e431
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-05-26 01:01:55 +0300

    02. CSharp Conditiona Statements and Loops - Exercises

---
+++ Tech Module/Programming Fundamentals/02. CSharp Conditiona  Statements and Loops - Exercises/03. Restaurant Discount/Restaurant Discount.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _03._Restaurant_Discount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int groupSize = int.Parse(Console.ReadLine());
+            string package = Console.ReadLine();
+
+            string hall = "";
+            double price = 0;
+            double discount = 0;
+
+            if (groupSize <= 50)
+            {
+                hall += "Small Hall";
+                price += 2500;
+            }
+            else if (groupSize <= 100)
+            {
+                hall += "Terrace";
+                price += 5000;
+            }
+            else if (groupSize <= 120)
+            {
+                hall += "Great Hall";
+                price += 7500;
+            }
+            else
+	        {
+                Console.WriteLine("We do not have an appropriate hall.");
+                return;
+            }
+
+            switch (package)
+            {
+                case "Normal":
+                    price += 500;
+                    discount += 0.95;
+                    break;
+                case "Gold":
+                    price += 750;
+                    discount += 0.90;
+                    break;
+                case "Platinum":
+                    price += 1000;
+                    discount += 0.85;
+                    break;
+            }
+            price *= discount;
+
+            double pricePerPerson = price / groupSize;
+
+            Console.WriteLine($"We can offer you the {hall}");
+            Console.WriteLine($"The price per person is {pricePerPerson:F2}$");
+        }
+    }
+}
