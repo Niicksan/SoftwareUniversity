8e2af240679ce4bf3ca49b0e262c2e8248871eca
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-05-26 01:06:02 +0300

    02. CSharp Conditiona Statements and Loops

---
+++ Tech Module/Programming Fundamentals/02. CSharp Conditiona  Statements and Loops - Exercises/09. Count the Integers/Count the Integers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _09._Count_the_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+
+            int counter = 0;
+
+            try
+            {
+                while (true)
+                {
+                    int num = int.Parse(input);
+                    counter++;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(counter);
+            }
+        }
+    }
+}
