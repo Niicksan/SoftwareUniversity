b5a9fa2cdaa47a8ee834cd8066cfdc4dc055d45d
Niicksan <nikslotar@abv.bg>
2018-07-03 00:49:02 +0300

    Fagaga

---
+++ 01. CSharp Intro and Basic Syntax - Exercises/01. Debit Card Number/Debit Card Number.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01._Debit_Card_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int fourtNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstNum:D4} {secondNum:D4} {thirdNum:D4} {fourtNum:D4} ");
+        }
+    }
+}
