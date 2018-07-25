2f9ad25cbffb6ac0b9620c9a0591a3f6de74239b
Niicksan <nikslotar@abv.bg>
2018-07-25 23:54:37 +0300

    PHP Syntax Basic Web - Exercises

---
+++ Tech Module/Software Technologies/Exercises/07. PHP-Syntax-Basic-Web-Exercises/exercises/factorial.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>First Steps Into PHP</title>
+
+</head>
+<body>
+<form>
+    N: <input type="text" name="num" />
+    <input type="submit" />
+</form>
+
+<?php
+    if (isset($_GET['num'])) {
+        $num = intval($_GET['num']);
+
+        $f0 = 0;
+        $f1 = 1;
+
+        echo $f1 . " ";
+        for ($i = 0; $i < $num - 1; $i++)
+        {
+            $fNext = $f0 + $f1;
+            $f0 = $f1;
+            $f1 = $fNext;
+
+            echo $f1 . " ";
+        }
+    }
+?>
+</body>
+</html>
