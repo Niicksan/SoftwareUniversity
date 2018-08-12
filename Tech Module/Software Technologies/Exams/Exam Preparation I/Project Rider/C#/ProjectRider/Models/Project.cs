07c727e26b577265e0e0741c4fb727103cdd58bf
Niicksan <nikslotar@abv.bg>
2018-08-12 22:58:45 +0300

    Exam Preparation

---
+++ Tech Module/Software Technologies/Exams/Exam Preparation I/Project Rider/C#/ProjectRider/Models/Project.cs
@@ -0,0 +1,22 @@
+﻿
+namespace ProjectRider.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Project
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(int.MinValue, int.MaxValue)]
+        public int Budget { get; set; }
+    }
+}
---
+++ Tech Module/Software Technologies/Exams/Exam Preparation II/IMDB/C# Skeleton/IMDB/Controllers/FilmController.cs
@@ -0,0 +1,79 @@
+﻿namespace IMDB.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Models;
+
+    public class FilmController : Controller
+    {
+        private readonly IMDBDbContext dbContext;
+
+        public FilmController(IMDBDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            List<Film> films = dbContext.Films.ToList();
+            return View(films);
+        }
+
+        [HttpGet]
+        [Route("/create")]
+        public IActionResult Create()
+        {
+            // TODO
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/create")]
+        public IActionResult Create(Film film)
+        {
+            dbContext.Films.Add(film);
+            dbContext.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        [Route("/edit/{id}")]
+        public IActionResult Edit(int? id)
+        {
+            Film film = dbContext.Films.FirstOrDefault(f => f.Id == id);
+            return View(film);
+        }
+
+        [HttpPost]
+        [Route("/edit/{id}")]
+        public IActionResult Edit(Film film)
+        {
+            dbContext.Update(film);
+            dbContext.SaveChanges();
+
+            return Redirect("/");
+        }
+
+        [HttpGet]
+        [Route("/delete/{id}")]
+        public IActionResult Delete(int? id)
+        {
+            Film film = dbContext.Films.Find(id);
+            return View(film);
+        }
+
+        [HttpPost]
+        [Route("/delete/{id}")]
+        public IActionResult Delete(Film film)
+        {
+            dbContext.Remove(film);
+            dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
