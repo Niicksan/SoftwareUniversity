bb2ed59e5d52349906f59176ce20e11a6ea1469e
Niicksan <nikslotar@abv.bg>
2018-08-12 23:40:46 +0300

    Issue Tracker

---
+++ Tech Module/Software Technologies/Exams/Issue Tracker/PHP/src/AppBundle/Controller/IssueController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Issue;
+use AppBundle\Form\IssueType;
+use AppBundle\Repository\IssueRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class IssueController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index(Request $request)
+    {
+        $issues = $this->getDoctrine()->getRepository(Issue::class)->findAll();
+        return $this->render('issue/index.html.twig', ['issues'=>$issues]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function create(Request $request)
+    {
+        $issue = new Issue();
+        $form = $this->createForm(IssueType::class,$issue);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()){
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($issue);
+            $entityManager->flush();
+
+            return $this->redirect('/');
+        }
+
+        return $this->render('issue/create.html.twig',['form'=>$form->createView()]);
+    }
+
+
+    /**
+     * @Route("/edit/{id}", name="edit")
+     * @param $id
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+
+    public function edit($id, Request $request)
+    {
+        $issue = $this->getDoctrine()->getRepository(Issue::class)->Find($id);
+        $form = $this->createForm(IssueType::class, $issue);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()){
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($issue);
+            $entityManager->flush();
+
+            return $this->redirect('/');
+        }
+
+        return $this->render('issue/edit.html.twig',['form'=>$form->createView(),'issue'=>$issue]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     * @param $id
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function delete($id, Request $request)
+    {
+        $issue = $this->getDoctrine()->getRepository(Issue::class)->Find($id);
+        $form = $this->createForm(IssueType::class, $issue);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()){
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($issue);
+            $entityManager->flush();
+
+            return $this->redirect('/');
+        }
+
+        return $this->render('issue/delete.html.twig',['form'=>$form->createView(),'issue'=>$issue]);
+    }
+}
