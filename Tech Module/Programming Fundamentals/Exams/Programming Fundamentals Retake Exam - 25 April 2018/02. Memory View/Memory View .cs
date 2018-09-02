8cad3bca249afbae9be74c996a276344d4c4e442
Niicksan <nikslotar@abv.bg>
2018-09-02 10:53:17 +0300

    Memory View

--- Tech Module/Programming Fundamentals/Exams/Programming Fundamentals Retake Exam - 25 April 2018/02. Memory View/Memory View .cs
+++ Tech Module/Programming Fundamentals/Exams/Programming Fundamentals Retake Exam - 25 April 2018/02. Memory View/Memory View .cs
@@ -6,7 +6,35 @@ namespace _02._Memory_View
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            string result = string.Empty;
+
+            List<string> words = new List<string>();
+
+            while (input != "Visual Studio crash")
+            {
+                result += input + " ";
+                input = Console.ReadLine();
+            }
+
+            string[] tokens = result.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < tokens.Length - 5; i++)
+            {
+                if (tokens[i] == "32656" && tokens[i + 1] == "19759" && tokens[i + 2] == "32763" && tokens[i + 3] == "0" && tokens[i + 5] == "0")
+                {
+                    string word = string.Empty;
+                    int wordLength = int.Parse(tokens[i + 4]);
+
+                    for (int j = i + 6; j <= i + 6 + wordLength; j++)
+                    {
+                        word += (char)(int.Parse(tokens[j]));
+                    }
+
+                    words.Add(word);
+                }
+            }
+
+            Console.WriteLine(string.Join("\n", words));
         }
     }
 }
