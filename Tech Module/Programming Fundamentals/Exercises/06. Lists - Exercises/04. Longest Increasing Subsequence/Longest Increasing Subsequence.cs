93fa3d71cd044f86f31ec2c401750268c3f35422
Niicksan <nikslotar@abv.bg>
2018-09-02 11:03:25 +0300

    Longest Increasing Subsequence

--- Tech Module/Programming Fundamentals/Exercises/06. Lists - Exercises/04. Longest Increasing Subsequence/Longest Increasing Subsequence.cs
+++ Tech Module/Programming Fundamentals/Exercises/06. Lists - Exercises/04. Longest Increasing Subsequence/Longest Increasing Subsequence.cs
@@ -7,92 +7,48 @@ namespace _04._Longest_Increasing_Subsequence
     class Program
     {
         static void Main(string[] args)
-        {   // Work but 66/100
+        {   // Works 100/100
             //input:
-            var nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> sequence = Console.ReadLine().Split().Select(int.Parse).ToList();
 
-            var seq = new List<int>();
-            var longestSeq = new List<int>();
-            seq.Add(nums[0]);
-            longestSeq.Add(nums[0]);
+            int[] length = new int[sequence.Count];
+            int[] prev = new int[sequence.Count];
+            int maxLength = 0;
+            int lastIndex = -1;
 
-            int longestSeqMax = nums[0];
-            int longestSeqMin = nums[0];
-            int currentSeqMin = nums[0];
-            int currentSeqMax = nums[0];
-
-            for (int index = 1; index < nums.Count(); index++)
+            for (int i = 0; i < sequence.Count; i++)
             {
-                int current = nums[index];
+                length[i] = 1;
+                prev[i] = 1;
 
-                if (current > currentSeqMin)
+                for (int j = 0; j < i; j++)
                 {
-                    if (current > currentSeqMax)
+                    if (sequence[j] < sequence[i] && length[j] >= length[i])
                     {
-                        seq.Add(current);
-                        currentSeqMax = current;
+                        length[i] = 1 + length[j];
+                        prev[i] = j;
                     }
-                    else if (current < currentSeqMax && (seq.IndexOf(current) == -1))
-                    {
-                        seq = seq.Where(n => n < current).ToList();
-                        seq.Add(current);
-                        currentSeqMax = current;
-                    }
-
-                    if (current > longestSeqMax)
-                    {
-                        longestSeq.Add(current);
-                        longestSeqMax = current;
-                    }
-
-                    if (seq.Count() > longestSeq.Count())
-                    {
-                        longestSeq.Clear();
-                        longestSeq = seq.Skip(0).Take(seq.Count()).ToList();
-                        longestSeqMin = longestSeq[0];
-                        longestSeqMax = longestSeq[longestSeq.Count() - 1];
-                    }
-
                 }
-                else
-                {
-                    if (seq.Count() > longestSeq.Count())
-                    {
-                        longestSeq.Clear();
-                        longestSeq = seq.Skip(0).Take(seq.Count()).ToList();
-                        longestSeqMax = longestSeq[longestSeq.Count() - 1];
-                        longestSeqMin = longestSeq[0];
-                    }
 
-                    seq.Clear();
-                    seq.Add(current);
-                    currentSeqMin = current;
-                    currentSeqMax = current;
+                if (length[i] > maxLength)
+                {
+                    maxLength = length[i];
+                    lastIndex = i;
                 }
             }
-           
+
+            List<int> longestSeq = new List<int>();
+            for (int i = 0; i < maxLength; i++)
+            {
+                longestSeq.Add(sequence[lastIndex]);
+                lastIndex = prev[lastIndex];
+            }
+
+            longestSeq.Reverse();
+
             Console.WriteLine(string.Join(" ", longestSeq));
 
             //goto input;
-        }
-
-        static bool HasChanceToIncrease(int startIndex, int number, List<int> nums)
-        {
-            if (startIndex == nums.Count() - 1)
-            {
-                if (number < nums[startIndex])
-                {
-                    return true;
-                }
-            }
-            for (int index = startIndex; index < nums.Count(); index++)
-            {
-                if (number < nums[index])
-                {
-                    return true;
-                }
-            }
-            return false;
         }
     }
 }
