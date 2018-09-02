d3f5a7d36b8b18e55a31be0fa1b989e188c0b362
Niicksan <nikslotar@abv.bg>
2018-09-02 11:07:49 +0300

    Bomb Numbers

--- Tech Module/Programming Fundamentals/Exercises/06. Lists - Exercises/07. Bomb Numbers/Bomb Numbers.cs
+++ Tech Module/Programming Fundamentals/Exercises/06. Lists - Exercises/07. Bomb Numbers/Bomb Numbers.cs
@@ -7,62 +7,66 @@ namespace _07._Bomb_Numbers
     class Program
     {
         static void Main(string[] args)
-        {   // Work but 60/100
+        {   // Works 100/100
             List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
-            List<int> bombNumbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int[] bombNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-            BombTheNumbers(numbers, bombNumbers);
+            int bomb = bombNumbers[0];
+            int power = bombNumbers[1];
 
-            int sum = SumNumbers(numbers);
-
-            Console.WriteLine(sum);
-        }
-
-        static int SumNumbers(List<int> numbers)
-        {
             int sum = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int leftIndex = i - power;
+                int rightIndex = i + 1;
+                int leftPower = power;
+                int rightPower = power;
+
+                if (numbers[i] == bomb)
+                {
+                    if (leftIndex <= 0)
+                    {
+                        leftIndex = 0;
+                    }
+
+                    if (i == numbers.Count)
+                    {
+                        leftIndex = 0;
+                        rightIndex = 0;
+                        leftPower = 0;
+                        rightIndex = 0;
+                    }
+
+                    if (i - power <= 0)
+                    {
+                        leftPower = i;
+                    }
+
+                    if (i + power >= numbers.Count)
+                    {
+                        rightPower = numbers.Count - 1 - i;
+                    }
+
+                    if (rightIndex >= numbers.Count)
+                    {
+                        rightIndex = numbers.Count - 1 - i;
+                    }
+
+                    numbers.RemoveRange(rightIndex, rightPower);
+                    numbers.RemoveRange(leftIndex, leftPower);
+                    numbers.Remove(bomb);
+
+                    i = 0;
+                }
+            }
+
             for (int i = 0; i < numbers.Count; i++)
             {
                 sum += numbers[i];
             }
 
-            return sum;
-        }
-
-        static void BombTheNumbers(List<int> numbers, List<int> bombNumbers)
-        {
-
-            int bomb = bombNumbers[0];
-            int power = bombNumbers[1];
-
-            for (int i = 0; i < numbers.Count - 1; i++)
-            {
-                int leftIndex = i - power;
-                int rightIndex = i + 1;
-                int leftPower = power + 1;
-
-                if (numbers[i] == bomb)
-                {
-                    if (i == numbers.Count)
-                    {
-                        power = 0;
-                    }
-
-                    if (i + power >= numbers.Count)
-                    {
-                        power = numbers.Count - i - 1;
-                    }
-
-                    numbers.RemoveRange(rightIndex, power);
-
-                    if (leftIndex < 0)
-                    {
-                        leftIndex = 0;
-                    }
-
-                    numbers.RemoveRange(leftIndex, leftPower);
-                }
-            }
+            Console.WriteLine(sum);
         }
     }
 } 
