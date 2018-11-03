f5473105078963de580a6f1f077ad9fc1bedc268
Niicksan <34044055+Niicksan@users.noreply.github.com>
2018-11-03 17:41:43 +0200

    Inheritance - Exercise

---
+++ Professional Modules/C# Fundamentals/C# OOP Basics/Exercises/03. Mankind/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Mankind
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] studentInfo = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] workerInfo = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            try
+            {
+                string studentFirstName = studentInfo[0];
+                string studentLasttName = studentInfo[1];
+                string facultyNumber = studentInfo[2];
+
+                Student student = new Student(studentFirstName, studentLasttName, facultyNumber);
+
+                string workerFirstName = workerInfo[0];
+                string workerLasttName = workerInfo[1];
+                decimal weekSalary = decimal.Parse(workerInfo[2]);
+                decimal workingHourPerDay = decimal.Parse(workerInfo[3]);
+
+                Worker worker = new Worker(workerFirstName, workerLasttName, weekSalary, workingHourPerDay);
+
+                Console.WriteLine(student.ToString());
+                Console.WriteLine(worker.ToString());
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
