3a40576f20888fd1ce4c2a9132474941e3763efb
Niicksan <nikslotar@abv.bg>
2018-11-17 20:50:12 +0200

    C# OOP Basics Exam Retake - 26 April 2018

---
+++ Professional Modules/C# Fundamentals/C# OOP Basics/Exams/CSharp OOP Basics Exam Retake - 26 April 2018/StorageMaster/Entities/Storages/Warehouse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StorageMaster.Entities.Vehicles;
+
+namespace StorageMaster.Entities.Storages
+{
+    public class Warehouse : Storage
+    {
+        private const int WarehouseCapacity = 10;
+
+        private const int WarehouseGarageSlots = 10;
+
+        private static Vehicle[] DefaultVehicles = {
+            new Semi(),
+            new Semi(),
+            new Semi()
+        };
+
+        public Warehouse(string name)
+            : base(name, WarehouseCapacity, WarehouseGarageSlots, DefaultVehicles)
+        {
+        }
+    }
+}
---
+++ Professional Modules/C# Fundamentals/C# OOP Basics/Exams/CSharp OOP Basics Exam Retake - 26 April 2018/StorageMaster/Factories/VehicleFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StorageMaster.Entities.Vehicles;
+
+namespace StorageMaster.Factories
+{
+    public class VehicleFactory
+    {
+        public Vehicle CreateVehicle(string type)
+        {
+            Vehicle vehicle = null;
+
+            switch (type)
+            {
+                case "Semi":
+                    vehicle = new Semi();
+                    break;
+                case "Truck":
+                    vehicle = new Truck();
+                    break;
+                case "Van":
+                    vehicle = new Van();
+                    break;
+                default:
+                    throw new InvalidOperationException("Invalid vehicle type!");
+            }
+
+            return vehicle;
+        }
+    }
+}
