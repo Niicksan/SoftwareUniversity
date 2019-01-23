637171ac664a11d58bd9d396eb065b548dbc6000
Niicksan <34044055+Niicksan@users.noreply.github.com>
2019-01-23 22:42:30 +0200

    Basic CRUD - Exersice

---
+++ Professional Modules/C# DB Fundamentals/Databases Basics - MS SQL Server/Exercises/02. Basic CRUD - Exercise/23. Biggest Countries by Population.sql
@@ -0,0 +1,4 @@
+SELECT TOP(30) CountryName, [Population] FROM Countries
+  WHERE ContinentCode = (SELECT ContinentCode FROM Continents
+  WHERE ContinentName = 'Europe')
+    ORDER BY Population DESC,CountryName;
