de8a0db955fb917c093c4c68c28eda07a482650c
Niicksan <34044055+Niicksan@users.noreply.github.com>
2019-08-11 12:06:10 +0300

    Exam Preparation - Panda App

---
+++ Professional Modules/C# Web/C# Web Basics/Exam Preparation/src/Apps/Panda/Panda.Data/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Panda.Data.Models
+{
+    public class User
+    {
+        public User()
+        {
+            this.Packages = new HashSet<Package>();
+            this.Receipts = new HashSet<Receipt>();
+        }
+
+        //[Key]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public virtual ICollection<Package> Packages { get; set; }
+
+        public virtual ICollection<Receipt> Receipts { get; set; }
+    }
+}
